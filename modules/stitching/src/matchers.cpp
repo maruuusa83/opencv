7785c834b049596b7efb15d67934401816fd5473
Daichi Teruya <tmarusa83@gmail.com>
2016-02-17 09:26:17 -0500

    change a type of a variable from int to size_t

--- modules/stitching/src/matchers.cpp
+++ modules/stitching/src/matchers.cpp
@@ -713,7 +713,7 @@ void BestOf2NearestOfNeighborMatcher::operator ()(const std::vector<ImageFeature
 
     std::vector<std::pair<int,int> > near_pairs;
     for (int i = 0; i < num_images - 1; ++i)
-        for (int j = 0; j < adjacencies_list_.size(); j++)
+        for (size_t j = 0; j < adjacencies_list_.size(); j++)
             if (features[i].keypoints.size() > 0 && features[j].keypoints.size() > 0 && mask_(i, j))
                 near_pairs.push_back(std::make_pair(i, j));
 
